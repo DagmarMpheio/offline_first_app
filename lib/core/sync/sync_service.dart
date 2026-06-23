34cf0fd013b26be23bde4ff79971ddfcda7335a9
Dagmar de Matos Mpheio <43245323+DagmarMpheio@users.noreply.github.com>
2026-06-23 22:16:39 +0100

    vai faltar sincronização bidirecional

--- lib/core/sync/sync_service.dart
+++ lib/core/sync/sync_service.dart
@@ -3,55 +3,7 @@ import 'dart:developer';
 import 'package:offline_first_app/features/clientes/data/models/cliente.dart';
 import 'package:offline_first_app/features/clientes/data/repositories/cliente_repository.dart';
 
-
 class SyncService {
-  final ClienteRepository repository;
-
-  bool _isSyncing = false;
-
-  SyncService(this.repository);
-
-  Future<void> sync() async {
-    if (_isSyncing) return;
-
-    _isSyncing = true;
-
-    try {
-      final pendentes =
-          await repository.getPendingSync();
-
-      for (final cliente in pendentes) {
-        await _syncCliente(cliente);
-      }
-    } catch (e) {
-      log('Erro ao sincronizar: $e');
-    } finally {
-      _isSyncing = false;
-    }
-  }
-
-  Future<void> _syncCliente(
-    Cliente cliente,
-  ) async {
-    if (cliente.isDeleted) {
-      await repository.remote
-          .deleteCliente(cliente.id);
-
-      await cliente.delete();
-
-      return;
-    }
-
-    await repository.remote
-        .saveCliente(cliente);
-
-    cliente.isSynced = true;
-
-    await cliente.save();
-  }
-}
-
-/* class SyncService {
   final ClienteRepository repository;
 
   bool _isSyncing = false;
@@ -75,4 +27,4 @@ class SyncService {
       _isSyncing = false;
     }
   }
-} */
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:offline_first_app/core/database/hive_service.dart';
+import 'package:offline_first_app/core/sync/sync_listener.dart';
 import 'package:offline_first_app/features/clientes/presentation/pages/clientes_page.dart';
+import 'package:offline_first_app/features/clientes/providers/sync_provider.dart';
 import 'package:offline_first_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await HiveService.init();
 
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    ProviderScope(
+      child: SyncListener(child: AppInitializer(child: MyApp())),
+    ),
+  );
 }
 
 class MyApp extends ConsumerStatefulWidget {
@@ -34,5 +38,30 @@ class _MyAppState extends ConsumerState<MyApp> {
       title: 'Offline First',
       home: const ClientesPage(),
     );
+  }
+}
+
+class AppInitializer extends ConsumerStatefulWidget {
+  const AppInitializer({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  ConsumerState<AppInitializer> createState() => _AppInitializerState();
+}
+
+class _AppInitializerState extends ConsumerState<AppInitializer> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() async {
+      await ref.read(syncServiceProvider).sync();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
   }
 }
